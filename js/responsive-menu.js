626e3383f113e19415e42a422f433428e1d3e3da
Santiago Emanuel Mustafa Font <santiagomustafa3@gmail.com>
2023-11-19 10:36:23 -0300

    Add animations and adit button to go to start de website.

--- js/responsive-menu.js
+++ js/responsive-menu.js
@@ -23,10 +23,30 @@ document.addEventListener("DOMContentLoaded", () => {
     })
 
     // Con este código evito que la navegación de celular aparezca cuando pasamos los 990px, si es que tiene la clase flex, que es donde comienza el diseño responsive para tablet y celular.
+    // Detecto cuando la página cambia su tamaño
     window.addEventListener("resize", () => {
+        // Detecto cada vez que su ancho es modificado y corroboro que sea mayor a 990px
         if (window.innerWidth > responsiveSize) {
+            // Si es mayor, oculto forzadamente el menú responsive, para que éste no cause problemas en el diseño.
             menu.classList.add("hidden")
             menu.classList.remove("flex")
+        }
+    })
+
+    // Obtengo el botón para volver al inicio de la página.
+    const up = document.getElementById("up");
+
+    // Detecto cada vez que se realiza scrolling en la página
+    window.addEventListener("scroll", () => {
+        // Detecto cuando el scrolling es mayor a 550
+        if (window.scrollY > 550) {
+            // Si es mayor, muestro el botón para volver al inicio de la página
+            up.classList.add("flex")
+            up.classList.remove("hidden")
+        } else {
+            // Caso contrario éste es ocultado.
+            up.classList.add("hidden")
+            up.classList.remove("flex")
         }
     })
 
