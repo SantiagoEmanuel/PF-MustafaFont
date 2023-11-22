54259c4052b08fb992732c6af07fd844227983b0
Santiago Emanuel Mustafa Font <santiagomustafa3@gmail.com>
2023-11-22 16:09:31 -0300

    Agrego cambios, código de js, html, seo

--- js/index.js
+++ js/index.js
@@ -33,21 +33,4 @@ document.addEventListener("DOMContentLoaded", () => {
         }
     })
 
-    // Obtengo el botón para volver al inicio de la página.
-    const up = document.getElementById("up");
-
-    // Detecto cada vez que se realiza scrolling en la página
-    window.addEventListener("scroll", () => {
-        // Detecto cuando el scrolling es mayor a 550
-        if (window.scrollY > 550) {
-            // Si es mayor, muestro el botón para volver al inicio de la página
-            up.classList.add("flex")
-            up.classList.remove("hidden")
-        } else {
-            // Caso contrario éste es ocultado.
-            up.classList.add("hidden")
-            up.classList.remove("flex")
-        }
-    })
-
 })
